791483345312eaa1f8589336d4bd23ff631ae0d0
OrestasM <m.orestas@yahoo.com>
2018-06-30 16:16:47 +0300

    Added jQuery, bootstrap to Jade

---
+++ public/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+//Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//Set Static Path
+app.use(express.static(path.join(__dirname,'public')));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+app.use('/jquery', express.static(__dirname + "/node_modules/jquery/dist/"))
+
+app.get('/',function(req,res){
+    res.render('index');
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000...");
+});
