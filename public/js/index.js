2d845c130aceb1d6106f0a81a44027a6b07c0504
OrestasM <m.orestas@yahoo.com>
2018-07-17 12:56:23 +0300

    fixed responsivenes

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,10 @@
 $('.navbar').toggleClass('solid', $(document).scrollTop() > 100);
 
-$(window).scroll(function(){
-    $('.navbar').toggleClass('solid', $(this).scrollTop() > 100);
-});
+if($(window).width() <=1000){
+    $('.navbar').addClass('solid');
+}else{
+    $(window).scroll(function(){
+        $('.navbar').toggleClass('solid', $(this).scrollTop() > 100);  
+    });
+}
+
